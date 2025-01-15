985bae638c1503087359da27f765ff684a4dab13
musmanirfan <usmanmala356@gmail.com>
2025-01-16 04:34:05 +0500

    adding pricing below section

--- include/header.php
+++ include/header.php
@@ -12,6 +12,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <?php include 'include/css.php'; ?>
     <title><?php echo isset($title) ? $title : 'Home'; ?></title>
     <meta name="google-site-verification" content="eoQvGFbRf4tINHldCoPIUiDqaD8icW2ghKB_3s__4Og" />
